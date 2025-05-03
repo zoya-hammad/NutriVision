ca1146498b9621edd689b0e3eebbfbe542e55212
Zoya Hammad <zoyahammadk@gmail.com>
2025-05-03 17:13:53 +0500

    Added class variables, added link functionality

--- mobile/app/src/main/java/com/example/nutrivisionapp/MainActivity.kt
+++ mobile/app/src/main/java/com/example/nutrivisionapp/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.nutrivisionapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var username :TextInputEditText
+    lateinit var password :TextInputEditText
+    lateinit var textSignUp : TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        initialiseVars()
+        setupSignUpLink()
+        Toast.makeText(this,"username: $username, pass: $password", Toast.LENGTH_LONG).show()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun initialiseVars() {
+        username = findViewById(R.id.edit_text_username)
+        password = findViewById(R.id.edit_text_password)
+    }
+
+    private fun setupSignUpLink() {
+        textSignUp = findViewById(R.id.text_view_sign_up)
+        textSignUp.setOnClickListener{
+            val intent = Intent(this@MainActivity, SignUp::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- mobile/app/src/main/java/com/example/nutrivisionapp/SignUp.kt
+++ mobile/app/src/main/java/com/example/nutrivisionapp/SignUp.kt
@@ -1,20 +1,52 @@
 package com.example.nutrivisionapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class SignUp : AppCompatActivity() {
+
+    lateinit var username : TextInputEditText
+    lateinit var email : TextInputEditText
+    lateinit var password : TextInputEditText
+    lateinit var passwordCheck : TextInputEditText
+    lateinit var textSignIn : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
+        initialiseVars()
+        setupSignInLink()
+        Toast.makeText(this,"user: $username, email: $email, pass: $password, confirmpass: $passwordCheck",Toast.LENGTH_LONG).show()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+
+    private fun initialiseVars() {
+        username = findViewById(R.id.edit_text_username)
+        email = findViewById(R.id.edit_text_email)
+        password = findViewById(R.id.edit_text_password)
+        passwordCheck = findViewById(R.id.edit_text_confirm_password)
+
+    }
+
+    private fun setupSignInLink() {
+        textSignIn = findViewById(R.id.text_view_sign_in)
+        textSignIn.setOnClickListener{
+            val intent = Intent(this@SignUp, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
