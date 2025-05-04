0a87232a182c6d2c5e7ff05917a4e1aea4bd0687
Zoya Hammad <zoyahammadk@gmail.com>
2025-05-04 22:07:36 +0500

    Added Homescreen.kt

---
+++ mobile/app/src/main/java/com/example/nutrivisionapp/HomeScreen.kt
@@ -0,0 +1,25 @@
+package com.example.nutrivisionapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeScreen : AppCompatActivity() {
+    lateinit var usernameTextView : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_screen)
+        val username = intent.getStringExtra("Username")
+        usernameTextView = findViewById(R.id.text_view_welcome_user)
+        usernameTextView.setText("Hi, $username")
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
