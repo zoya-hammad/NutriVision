9661386f5d1d058fd878a4e26e7005d8aeedba9f
Zoya Hammad <zoyahammadk@gmail.com>
2025-05-28 20:03:24 +0500

    Updated base_agent.py

--- nutritional_assistant/agents/base_agent.py
+++ nutritional_assistant/agents/base_agent.py
@@ -1,35 +1,22 @@
 import logging
 
+logging.basicConfig(
+    level=logging.INFO,  
+    format='%(message)s' 
+)
+
 class BaseAgent:
     """
     An abstract superclass for Agents
     Used to log messages in a way that can identify each Agent
     """
 
-    # Foreground colors
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
-    WHITE = '\033[37m'
-    BRIGHT_MAGENTA = '\033[95m'
-    
-    # Background color
-    BG_BLACK = '\033[40m'
-    
-    # Reset code to return to default color
-    RESET = '\033[0m'
-
-    def __init__(self, name: str, color: str = WHITE):
+    def __init__(self, name: str):
         self.name = name
-        self.color = color
 
     def log(self, message: str):
         """
         Log this as an info message, identifying the agent
         """
-        color_code = self.BG_BLACK + self.color
         message = f"[{self.name}] {message}"
-        logging.info(color_code + message + self.RESET) 
+        logging.info(message) 
